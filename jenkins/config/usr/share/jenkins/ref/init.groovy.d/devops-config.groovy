0ab4b296d762d8a52f7b3c7f1e3853e327188fbc
Anthony Angelo <anthony.angelo@servicenow.com>
2019-12-04 00:15:48 -0500

    Update

---
+++ jenkins/config/usr/share/jenkins/ref/init.groovy.d/devops-config.groovy
@@ -0,0 +1,28 @@
+import jenkins.model.*
+
+// Local Variables
+String apiVersion = "v1"
+
+// Get System Environment
+def env = System.getenv()
+
+// Get Jenkins Instance
+def jenkins = Jenkins.getInstance()
+
+// Get DevOps Plugin
+def devops = jenkins.getDescriptor("io.jenkins.plugins.config.DevOpsConfiguration")
+
+// Set DevOps Plugin Properties
+devops.snDevopsEnabled=true
+devops.instanceUrl=env.NOW_URL
+devops.apiVersion=apiVersion
+devops.user=env.NOW_USER
+devops.pwd=env.NOW_PASSWORD
+devops.toolId=NOW_TOOLID
+devops.debug=false
+
+// Save Plugin
+devops.save()
+
+// Save Config
+jenkins.save()

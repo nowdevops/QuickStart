1c1e63c0c31c2b89eb63474da289bde54e2e88c4
Anthony Angelo <anthony.angelo@servicenow.com>
2019-12-05 22:32:50 -0500

    Update

--- jenkins/config/usr/share/jenkins/ref/init.groovy.d/04-config-devops.groovy
+++ jenkins/config/usr/share/jenkins/ref/init.groovy.d/04-config-devops.groovy
@@ -13,9 +13,13 @@ Jenkins jenkins = Jenkins.getInstance()
 // Get DevOps Plugin
 def devops = jenkins.getDescriptor("io.jenkins.plugins.config.DevOpsConfiguration")
 
+// Test Config
+logger.info("Testing ServiceNow Connection")
+devops.doTestConnection(env.NOW_URL,env.NOW_API_VERSION,env.NOW_USER,env.NOW_PASSWORD,env.NOW_TOOLID)
+
 // Set DevOps Plugin Properties
 logger.info("Configuring DevOps Plugin")
-devops.snDevopsEnabled=false
+devops.snDevopsEnabled=true
 devops.instanceUrl=env.NOW_URL
 devops.apiVersion=env.NOW_API_VERSION
 devops.user=env.NOW_USER
@@ -23,10 +27,7 @@ devops.pwd=env.NOW_PASSWORD
 devops.toolId=env.NOW_TOOLID
 devops.debug=false
 
-// Test Config
-logger.info("Testing ServiceNow Connection")
-devops.doTestConnection(env.NOW_URL,env.NOW_API_VERSION,env.NOW_USER,env.NOW_PASSWORD,env.NOW_TOOLID)
-
 // Save Config
 logger.info("Saving Jenkins Configuration")
 devops.save()
+jenkins.save()
